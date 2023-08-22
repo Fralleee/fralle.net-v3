856130b0cf12db432139b600ea67945fade18684
Roland Chelwing <jolleee@hotmail.com>
2023-08-22 20:43:33 +0200

    Fix tracker issue

--- components/tracker.tsx
+++ components/tracker.tsx
@@ -27,6 +27,10 @@ export default function Tracker() {
     };
 
     const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    if (colorSchemeQuery.matches) {
+      setColorScheme("dark");
+    }
+
     colorSchemeQuery.addEventListener("change", handleColorSchemeChange);
 
     return () => {
@@ -35,10 +39,9 @@ export default function Tracker() {
   }, []);
 
   const useTracker = colorScheme === "dark";
-
   return useTracker ? (
     <div
-      className="pointer-events-none fixed inset-0 -z-10 transition duration-300"
+      className="pointer-events-none fixed inset-0 -z-10 transition duration-300 hidden lg:block"
       style={{
         background: `radial-gradient(600px at ${gradientPosition.x}px ${gradientPosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
       }}
