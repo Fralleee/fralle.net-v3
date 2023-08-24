c378a9c6523b61a0a3308fb906b490da3fc93775
Roland Chelwing <roland.chelwing@gmail.com>
2023-08-24 18:28:56 +0000

    Change color of the nav links

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -22,23 +22,21 @@ export default function NavLink({ link, title, active, onClick }: NavLinkProps) 
         bg-slate-300 group-hover:bg-blue-500 group-focus-visible:bg-blue-500
         dark:bg-slate-500 dark:group-hover:bg-amber-300 dark:group-focus-visible:bg-amber-300`,
             active && "w-48 h-8",
-            active && "bg-blue-500",
-            active && "dark:bg-slate-300"
+            active && "bg-blue-200",
+            active && "dark:bg-slate-800"
           )}
         />
         <span
           className={cn(
             `nav-text text-xs font-bold uppercase tracking-widest transition-all motion-reduce:transition-none
 
-        group-hover:-translate-x-48  group-focus-visible:-translate-x-48
-        group-hover:ml-2  group-focus-visible:ml-2
+        group-hover:-translate-x-48 group-focus-visible:-translate-x-48
+        group-hover:ml-2 group-focus-visible:ml-2
 
-        text-slate-400 group-hover:text-blue-500 group-focus-visible:text-blue-500
+        text-slate-700 group-hover:text-blue-200 group-focus-visible:text-blue-200
         dark:text-slate-300 dark:group-hover:text-slate-700 dark:group-focus-visible:text-slate-700`,
             active && "-translate-x-48",
-            active && "ml-2",
-            active && "text-blue-500",
-            active && "dark:text-slate-700"
+            active && "ml-2"
           )}>
           {title}
         </span>
