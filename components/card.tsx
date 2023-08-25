dc34eb58b68751baf3dc55f455bd904bcd233927
Roland Chelwing <jolleee@hotmail.com>
2023-08-25 18:36:12 +0200

    Lower shadow hover

--- components/card.tsx
+++ components/card.tsx
@@ -6,7 +6,7 @@ export default function Card({ children }: { children: React.ReactNode }) {
       lg:group-hover:bg-slate-50 
       lg:group-hover:drop-shadow-[0_0_1px_rgba(0,0,0,0.15)]
 
-      dark:lg:group-hover:bg-slate-950 
+      dark:lg:group-hover:bg-slate-900 
       dark:lg:group-hover:shadow-[inset_0_-1px_0_0_rgba(148,163,184,0.2)] 
       
       "></div>
