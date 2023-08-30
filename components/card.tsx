227a2ef54d31ec93d5f7d91ee6b742ff642afc1e
Roland Chelwing <jolleee@hotmail.com>
2023-08-30 14:22:18 +0200

    Add more padding to card

--- components/card.tsx
+++ components/card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+    <div className="group relative grid pb-4 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
       {children}
     </div>
   );
