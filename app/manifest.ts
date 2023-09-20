6f8f77efc193c2bacad6202a99652a796a1110c8
Roland Chelwing <jolleee@hotmail.com>
2023-09-20 15:38:56 +0200

    New logo for cooking site

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,10 +11,54 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: "#101E3F",
         icons: [
             {
-                src: "/favicon.ico",
-                sizes: "any",
-                type: "image/x-icon",
+                src: "/icons/icon-72x72.png",
+                sizes: "72x72",
+                type: "image/png",
             },
+            {
+                src: "/icons/icon-96x96.png",
+                sizes: "96x96",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-128x128.png",
+                sizes: "128x128",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-144x144.png",
+                sizes: "144x144",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-152x152.png",
+                sizes: "152x152",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-384x384.png",
+                sizes: "384x384",
+                type: "image/png",
+            },
+            {
+                src: "/icons/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
+        scope: "/",
+        categories: [
+            "Portfolio",
+            "Personalization",
+            "Productivity",
+            "Education",
+            "Entertainment",
+            "Social",
         ],
     };
 }
