63084febd5d0e78f44d7470cfc754ce3f3406935
Roland Chelwing <jolleee@hotmail.com>
2023-09-28 10:11:43 +0200

    Update tech in CWF

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://fralle.net",
+            url: "https://www.fralle.net",
             lastModified: new Date(),
         },
     ];
