83261b88c1cc295a5ef0bb8e8a050749a1f98aae
Roland Chelwing <jolleee@hotmail.com>
2025-06-07 15:27:30 +0200

    chore: add knip

--- providers/posthog.tsx
+++ providers/posthog.tsx
@@ -2,23 +2,23 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 if (typeof window !== "undefined") {
-  const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-  const host = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+	const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+	const host = process.env.NEXT_PUBLIC_POSTHOG_HOST;
 
-  if (!key) {
-    console.error("PostHog key is missing");
-  } else if (!host) {
-    console.error("PostHog host is missing");
-  } else {
-    posthog.init(key, {
-      api_host: host,
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    });
-  }
+	if (!key) {
+		console.error("PostHog key is missing");
+	} else if (!host) {
+		console.error("PostHog host is missing");
+	} else {
+		posthog.init(key, {
+			api_host: host,
+			person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+		});
+	}
 }
 export function CSPostHogProvider({ children }: PropsWithChildren<unknown>) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+	return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
