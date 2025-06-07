805d72a1fb13b9d6339c3f862d2dee4f907b7d09
Roland Chelwing <jolleee@hotmail.com>
2025-06-07 17:53:30 +0200

    chore: cleanup provider

--- providers/posthog.tsx
+++ providers/posthog.tsx
@@ -2,23 +2,23 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import type { PropsWithChildren } from "react";
+import { useEffect, type PropsWithChildren } from "react";
 
-if (typeof window !== "undefined") {
-	const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-	const host = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+export function CSPostHogProvider({ children }: PropsWithChildren<unknown>) {
+	useEffect(() => {
+		const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+		const host = process.env.NEXT_PUBLIC_POSTHOG_HOST;
 
-	if (!key) {
-		console.error("PostHog key is missing");
-	} else if (!host) {
-		console.error("PostHog host is missing");
-	} else {
+		if (!key || !host) {
+			console.error("❌ PostHog key or host missing", { key, host });
+			return;
+		}
+
 		posthog.init(key, {
 			api_host: host,
-			person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+			capture_pageview: true,
+			person_profiles: "identified_only",
 		});
-	}
-}
-export function CSPostHogProvider({ children }: PropsWithChildren<unknown>) {
+	}, []);
 	return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
