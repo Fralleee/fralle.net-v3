f95c2027b376b1a47b9e71c6a53ca456ba3cadd7
Roland Chelwing <jolleee@hotmail.com>
2025-06-14 21:28:06 +0200

    chore: animate tooltip

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -43,7 +43,7 @@ function TooltipContent({
 	return (
 		<TooltipPrimitive.Content
 			className={cn(
-				"fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in text-balance rounded-lg bg-slate-950 px-3 py-2 text-primary-foreground text-sm data-[state=closed]:animate-out",
+				"TooltipContent z-50 w-fit origin-(--radix-tooltip-content-transform-origin) text-balance rounded-lg bg-slate-950 px-3 py-2 text-primary-foreground text-sm",
 				className,
 			)}
 			data-slot="tooltip-content"
